56578dc5106646234d58d759cf2f5761c1e5183e
Petr Hracek <phracek@redhat.com>
2015-09-02 21:38:26 +0200

    Run all preupgrade scripts and report which failed RHBZ #1252850
    Signed-off-by: Petr Hracek <phracek@redhat.com>

--- redhat-upgrade-tool.py
+++ redhat-upgrade-tool.py
@@ -336,13 +336,21 @@ def main(args):
     # Run the preuprade scripts if present
     if os.path.isdir(preupgrade_script_path):
         scripts = sorted(rlistdir(preupgrade_script_path))
+        failed = False
+        failed_scripts = {}
         for s in scripts:
             if os.access(s, os.X_OK):
                 try:
                     check_call(s)
                 except CalledProcessError as e:
-                    print("%s exited with status %d, exiting" % (s, e.returncode))
-                    sys.exit(1)
+                    failed_scripts[s] = e.returncode
+                    failed = True
+        if failed:
+            print("Following preupgrade script(s) failed:\n")
+            for key, val in failed_scripts:
+                print("%s exited with status %d" % (key, val))
+            print('exiting')
+            sys.exit(1)
 
     if not args.skipbootloader:
         if args.skipkernel:
